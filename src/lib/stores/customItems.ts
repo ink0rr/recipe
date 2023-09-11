789679d0caacd6f7900e8f877f750393d307e74c
ink0rr <tomflynnbeltsazar@gmail.com>
2023-09-12 02:33:49 +0700

    Change store

--- src/lib/stores/customItems.ts
+++ src/lib/stores/customItems.ts
@@ -1,21 +1,4 @@
-import { browser } from "$app/environment";
-import { writable } from "svelte/store";
+import { persisted } from "svelte-local-storage-store";
 import type { Item } from "../types/Item";
 
-function loadCustomItems(): Record<string, Item> {
-  if (!browser) return {};
-
-  const str = localStorage.getItem("customItems") ?? "{}";
-  try {
-    const items = JSON.parse(str);
-    if (typeof items !== "object" || Array.isArray(items)) {
-      throw new Error("Invalid custom items type");
-    }
-    return items;
-  } catch {
-    localStorage.setItem("customItems", "{}");
-    return {};
-  }
-}
-
-export const customItems = writable<Record<string, Item>>(loadCustomItems());
+export const customItems = persisted<Record<string, Item>>("customItems", {});
