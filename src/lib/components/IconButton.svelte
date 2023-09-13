2b321e759699ceba574da067b2e9e1baf1282871
ink0rr <tomflynnbeltsazar@gmail.com>
2023-09-14 04:25:47 +0700

    Add icon button component

---
+++ src/lib/components/IconButton.svelte
@@ -0,0 +1,7 @@
+<button
+  type="button"
+  class="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700 [&>*]:pointer-events-none"
+  on:click
+>
+  <slot />
+</button>
--- src/lib/components/SettingsModal.svelte
+++ src/lib/components/SettingsModal.svelte
@@ -2,18 +2,18 @@
   import { settings } from "$lib/stores/settings";
   import { Input, Label, Modal, Toggle } from "flowbite-svelte";
   import { CogOutline } from "flowbite-svelte-icons";
+  import IconButton from "./IconButton.svelte";
 
   let open = false;
 </script>
 
-<button
-  class="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+<IconButton
   on:click={() => {
     open = true;
   }}
 >
-  <CogOutline class="h-5 w-5" />
-</button>
+  <CogOutline />
+</IconButton>
 
 <Modal title="Settings" size="sm" outsideclose bind:open>
   <div class="flex flex-col gap-4">
