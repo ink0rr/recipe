46b08b01ae46d5bf3280c5363f913531190e6b58
ink0rr <tomflynnbeltsazar@gmail.com>
2023-09-15 08:49:58 +0700

    Add invalid namespace helper text

--- src/lib/components/SettingsModal.svelte
+++ src/lib/components/SettingsModal.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import { settings } from "$lib/stores/settings";
-  import { Input, Label, Modal, Toggle } from "flowbite-svelte";
+  import { Helper, Input, Label, Modal, Toggle } from "flowbite-svelte";
   import { CogOutline } from "flowbite-svelte-icons";
   import IconButton from "./IconButton.svelte";
 
   let open = false;
+  let namespace = $settings.namespace;
+  let invalidNamespace: string | false = false;
+
+  $: if (!open) {
+    namespace = namespace.trim().toLowerCase();
+    $settings.namespace = namespace;
+  }
+  $: if (namespace.match(/[ \W]/)) {
+    invalidNamespace = "Invalid namespace";
+  } else {
+    invalidNamespace = false;
+  }
 </script>
 
 <IconButton
@@ -19,7 +31,10 @@
   <div class="flex flex-col gap-4">
     <div class="flex flex-col gap-1">
       <Label>Default Namespace</Label>
-      <Input placeholder={"custom"} bind:value={$settings.namespace} />
+      <Input color={invalidNamespace ? "red" : "base"} spellcheck="false" bind:value={namespace} />
+      {#if invalidNamespace}
+        <Helper color="red">{invalidNamespace}</Helper>
+      {/if}
     </div>
     <Toggle bind:checked={$settings.compact}>Compact Result Image</Toggle>
   </div>
