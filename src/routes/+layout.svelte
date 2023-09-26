1ea7e8ea37ec48d036ba6a181e1074c7f4e0d266
ink0rr <tomflynnbeltsazar@gmail.com>
2023-09-26 20:09:25 +0700

    Change og image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,14 @@
 <script>
-  import { page } from "$app/stores";
   import "../styles.css";
 </script>
 
 <svelte:head>
   <title>Recipe Generator</title>
   <meta name="description" content="Recipe generator for Minecraft Bedrock" />
-  <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:title" content="Recipe Generator" />
-  <meta property="twitter:description" content="Recipe generator for Minecraft Bedrock" />
-  <meta property="twitter:image" content={`/image${$page.url.search}`} />
   <meta property="og:title" content="Recipe Generator" />
   <meta property="og:description" content="Recipe generator for Minecraft Bedrock" />
   <meta property="og:type" content="website" />
-  <meta property="og:image" content={`/image${$page.url.search}`} />
+  <meta property="og:image" content="/favicon.png" />
 </svelte:head>
 
 <div class="flex min-h-screen flex-col">
