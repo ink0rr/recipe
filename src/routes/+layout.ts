0f914ff5181dbf99c10a4ff2dc662bd42fab64da
ink0rr <tomflynnbeltsazar@gmail.com>
2023-12-17 17:08:53 +0700

    Add speed insight

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,6 @@
 import { dev } from "$app/environment";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 inject({ mode: dev ? "development" : "production" });
+injectSpeedInsights({});
