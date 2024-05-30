f1582c5293cbb33584e102c43b8aab78bf3998d4
ink0rr <tomflynnbeltsazar@gmail.com>
2024-05-30 11:50:21 +0700

    Update meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,16 @@
 
 <svelte:head>
   <title>Recipe Generator</title>
-  <meta name="description" content="Recipe generator for Minecraft Bedrock" />
+  <meta name="description" content="Crafting recipe generator for Minecraft Bedrock" />
   <meta property="og:title" content="Recipe Generator" />
-  <meta property="og:description" content="Recipe generator for Minecraft Bedrock" />
+  <meta property="og:description" content="Crafting recipe generator for Minecraft Bedrock" />
   <meta property="og:type" content="website" />
   <meta property="og:image" content="/favicon.png" />
+  <meta property="og:locale" content="en_US" />
+  <meta property="twitter:card" content="summary" />
+  <meta property="twitter:title" content="Recipe Generator" />
+  <meta property="twitter:description" content="Crafting recipe generator for Minecraft Bedrock" />
+  <meta property="twitter:image" content="/favicon.png" />
 </svelte:head>
 
 <div class="flex min-h-screen flex-col">
